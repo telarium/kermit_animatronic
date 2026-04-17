d1779b86d9e077dadb3f5604dcc295bb0620a7ad
telarium <andrew@langleycreations.com>
2026-04-17 15:02:49 -0700

    Todo messages

--- wifi_management.py
+++ wifi_management.py
@@ -1,6 +1,6 @@
 class WifiManagement:
 	def __init__(self, config_file: str = "config.cfg") -> None:
-		print("TODO")
+		print("TODO WIFI CLASS")
 
 	
 
@@ -9,4 +9,4 @@ if __name__ == "__main__":
 	ap_manager = WifiManagement()
 
 	def scan_wifi_access_points(self, signal_threshold: int = 30) -> Optional[List[Dict[str, Any]]]:
-		print("TODO")
+		print("TODO  WIFI SCAN")
