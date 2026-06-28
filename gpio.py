2635939fab6ec8538185b9075634c9ae93fdb0c2
telarium <andrew@langleycreations.com>
2026-06-28 14:54:23 -0700

    Removed unused i2c address

--- gpio.py
+++ gpio.py
@@ -43,7 +43,7 @@ class GPIO:
 			bus = smbus.SMBus(1)  # Initialize I2C bus
 
 			# I2C addresses of the MCP23008 devices
-			i2c_addresses = [0x20, 0x21, 0x23]
+			i2c_addresses = [0x20, 0x21]
 
 			# Initialize MCP23008 devices and store them in a list
 			self.mcp_devices = [MCP23008(bus, addr) for addr in i2c_addresses]
